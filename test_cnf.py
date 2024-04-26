3b8695c7442d8639f93a2122bd0a07e99a6ead19
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-04-26 17:13:32 +1000

    implemented cnf conversion tests

---
+++ test_cnf.py
@@ -0,0 +1,60 @@
+from cnf import eliminate_implications, move_not_inwards, distribute_and_over_or, to_cnf
+from utils import associate, dissociate
+from expr import Expr, expr, Symbol
+
+
+def test_dissociate():
+    expr1 = expr('a & b')
+    expr2 = expr('(a & b) & (c || b) & d')
+    expr3 = expr('(a <=> b) || (c ==> a) || c || d')
+
+    assert dissociate('&', expr1.args) == list(expr('a, b'))
+    assert dissociate('&', expr2.args) == list(expr('a, b, c || b, d'))
+    assert dissociate('||', expr3.args) == list(expr('a <=> b, c ==> a, c, d'))
+
+
+def test_associate():
+    expr1 = expr('(a & b) & (b || c) & (b & c)')
+    expr2 = expr('a || (b || (c || (a & b)))')
+
+    x = associate('||', expr2.args)
+    assert associate('&', expr1.args) == Expr('&', Symbol('a'), Symbol('b'), expr('(b || c)'), Symbol('b'), Symbol('c'))
+    assert associate('||', expr2.args) == Expr('||', Symbol('a'), Symbol('b'), Symbol('c'), expr('a & b'))
+
+
+def test_eliminate_implications():
+    expr1 = expr('a <=> b')
+    expr2 = expr('a ==> b')
+    expr3 = expr('((~a & b) || c) ==> d')
+
+    assert eliminate_implications(expr1) == expr('(~a || b) & (~b || a)')
+    assert eliminate_implications(expr2) == expr('~a || b')
+    assert eliminate_implications(expr3) == expr('~((~a & b) || c) || d')
+
+
+def test_move_not_inwards():
+    expr1 = expr('~(~a)')
+    expr2 = expr('~(a & b)')
+    expr3 = expr('~(a || b)')
+
+    assert move_not_inwards(expr1) == expr('a')
+    assert move_not_inwards(expr2) == expr('~a || ~b')
+    assert move_not_inwards(expr3) == expr('~a & ~ b')
+
+
+def test_distribute_and_over_or():
+    expr1 = expr('(a & b) || c')
+    expr2 = expr('((a ==> (b & c)) & d) || (b <=> d)')
+
+    assert distribute_and_over_or(expr1) == expr('(a || c) & (b || c)')
+    assert distribute_and_over_or(expr2) == expr('((a ==> (b & c)) || (b <=> d)) & (d || (b <=> d))')
+
+
+def test_to_conjunctive_normal_form():
+    expr1 = expr('~(b || c)')
+    expr2 = expr('a || (b & c)')
+    # expr3 = expr('a <=> (b || c)') # todo fix later
+
+    assert to_cnf(expr1) == expr('~b & ~c')
+    assert to_cnf(expr2) == expr('(b || a) & (c || a)')  # distributivity of || over &
+    # assert to_cnf(expr3) == expr('(~a || b || c) & (~b || a) & (~c || a)')
