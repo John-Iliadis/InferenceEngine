e61798966f5d537ce3074581376b7bcd4629a930
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-04-26 21:38:13 +1000

    implemented tt_entails

---
+++ entail_engine.py
@@ -0,0 +1,75 @@
+"""entail_engine.py: File containing entailment algorithms."""
+
+from expr import Expr, is_symbol, get_symbols
+from utils import extend
+from typing import Union
+
+
+def tt_entails(kb: 'Expr', query: 'Expr') -> bool:
+    symbols = list(get_symbols(kb & query))
+    return tt_check_all(kb, query, symbols, {})
+
+
+def tt_check_all(kb: 'Expr', query: 'Expr', symbols: list, model: dict) -> bool:
+    if not symbols:
+        if pl_true(kb, model):
+            result = pl_true(query, model)
+            assert result in [True, False]
+            return result
+        else:
+            return True
+    else:
+        p = symbols[0]
+        rest = symbols[1:]
+        return (tt_check_all(kb, query, rest, extend(model, p, True)) and
+                tt_check_all(kb, query, rest, extend(model, p, False)))
+
+
+def pl_true(exp: Union['Expr', bool], model: dict) -> Union[bool, None]:
+    """Returns true if the expression is true in the given model."""
+    if exp in [True, False]:
+        return exp
+
+    op = exp.op
+    args = exp.args
+
+    if is_symbol(op):
+        return model.get(exp)
+    elif op == '~':
+        p = pl_true(args[0], model)
+        return None if p is None else not p
+    elif op == '||':
+        result = False
+        for arg in args:
+            p = pl_true(arg, model)
+            if p is True:
+                return True
+            elif p is None:
+                result = None
+        return result
+    elif op == '&':
+        result = True
+        for arg in args:
+            p = pl_true(arg, model)
+            if p is False:
+                return False
+            if p is None:
+                result = None
+        return result
+
+    p, q = args
+
+    if op == '==>':
+        return pl_true(~p | '||' | q, model)
+
+    pt = pl_true(p, model)
+
+    if pt is None:
+        return None
+
+    qt = pl_true(q, model)
+
+    if op == '<=>':
+        return pt == qt
+
+    raise ValueError('Illegal operator in logic expression' + str(exp))
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from entail_engine import tt_entails
+from expr import kb2expr, expr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    kb = ['It_is_raining & ~I_have_an_umbrella => I_get_wet', 'It_is_raining']
+    query = 'I_get_wet'
+    print(tt_entails(kb2expr(kb), expr(query)))
