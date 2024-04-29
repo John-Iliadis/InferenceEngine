93b67f4a2e8168e08b43128e37c37cab86717964
field <97655247+field845@users.noreply.github.com>
2024-04-30 08:34:29 +1000

    finish bc

--- entail_engine.py
+++ entail_engine.py
@@ -97,23 +97,16 @@ def pl_fc_entails(kb, q) -> bool:
         fc_symbols.add(item)
 
     while agenda:
-        print(agenda)
         p = agenda.pop()
         if p == q:
-            print("fc_symbols: ")
-            print(fc_symbols)
-            fc_symbols.add(p)
             return True
         if not inferred[p]:
             inferred[p] = True
-            print(kb.clauses_with_premise(p))
             for c in kb.clauses_with_premise(p):
                 count[c] -= 1
                 fc_symbols.add(c.args[1])
                 if count[c] == 0:
                     agenda.append(c.args[1])
-                    print("fc_symbols: ")
-                    print(fc_symbols)
 
     return False
 
@@ -123,42 +116,35 @@ def get_fc_symbols():
 
 
 def pl_bc_entails(kb, q) -> bool:
-    """
-    [Figure 7.15]
-    Use forward chaining to see if a PropDefiniteKB entails symbol q.
-    >>> pl_fc_entails(horn_clauses_KB, expr('Q'))
-    True
-    """
-    count = {c: len(conjuncts(c.args[0])) for c in kb.clauses if c.op == '==>'}
-    # rhs = {c: conjuncts(c.args[1]) for c in kb.clauses if c.op == '==>'}
     inferred = defaultdict(bool)
     agenda = [q]
     prop_symbol = [s for s in kb.clauses if is_prop_symbol(s.op)]
-    for c in kb.clauses:
-        if c.op == '==>':
-            for c1 in conjuncts(c.args[0]):
-                symbol_list.add(c1)
-            for c2 in conjuncts(c.args[0]):
-                symbol_list.add(c2)
 
     while agenda:
+        print("agenda: ")
+        print(agenda)
         p = agenda.pop()
-        # if p in prop_symbol:
+        bc_symbols.append(p)
         if p in prop_symbol:
             return True
+        if not kb.clauses_by_conclusion(p):
+            return False
         if not inferred[p]:
             inferred[p] = True
-            bc_symbols.append(p)
+            if not kb.clauses_by_conclusion(p):
+                agenda.append(p)
+            print(kb.clauses_by_conclusion(p))
             for c in kb.clauses_by_conclusion(p):
-                print(kb.clauses_by_conclusion(p))
                 if c.op == '==>':
-                    for c2 in conjuncts(c.args[0]):
-                        agenda.append(c2)
-                        print("agenda: ")
-                        print(agenda)
+                    print(conjuncts(c.args[0]))
+                    bc_symbols.extend(conjuncts(c.args[0]))
+                    agenda.extend(conjuncts(c.args[0]))
+                    print("bc_symbols: ")
+                    print(bc_symbols)
 
     return False
 
 
 def get_bc_symbols():
-    return bc_symbols
+    bc_symbols_set = set(bc_symbols)
+    return bc_symbols_set
