4fe6340d0599e2403052d8726f892e2f9eac44c3
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-05-01 08:31:06 +1000

    renamed from test_entail_engine.py

--- test_inference_algorithms.py
+++ test_inference_algorithms.py
@@ -1,6 +1,6 @@
-"""test_entail_engine.py: Tests entailment algorithms."""
+"""test_inference_algorithms.py: Tests entailment algorithms."""
 
-from entail_engine import pl_true, tt_entails
+from inference_algorithms import pl_true, tt_entails
 from expr import Expr, expr
 
 
@@ -11,8 +11,7 @@ def test_pl_true():
     expr2 = expr('(magical & horned) ==> mythical')
     model_2 = {Expr('magical'): True, Expr('mythical'): False, Expr('horned'): True}
 
-    expr3 = expr(
-        '((smoke & heat) ==> fire) <=> ((smoke ==> fire) || (heat ==> fire))')
+    expr3 = expr('((smoke & heat) ==> fire) <=> ((smoke ==> fire) || (heat ==> fire))')
     model_3 = {Expr('smoke'): False, Expr('heat'): False, Expr('fire'): False}
 
     assert pl_true(expr1, model_1) is True
@@ -24,12 +23,12 @@ def test_tt_entails():
     kb1 = expr('A & (B || C) & D & E & ~(F || G)')
     query_1 = expr('A & D & E & ~F & ~G')
 
-    kb2 = expr('P || Q')
-    query_2 = expr('Q')
-
-    kb3 = expr('P & Q')
-    query_3 = expr('Q')
+    kb2 = expr('(My ==> I) & (~My ==> ~I & Mam) & ((I || Mam) ==> H) & (H ==> Ma)')
+    query_2 = expr('My')
+    query_3 = expr('Ma')
+    query_4 = expr('H')
 
     assert tt_entails(kb1, query_1) is True
     assert tt_entails(kb2, query_2) is False
-    assert tt_entails(kb3, query_3) is True
+    assert tt_entails(kb2, query_3) is True
+    assert tt_entails(kb2, query_4) is True
