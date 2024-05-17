a00a10eafcf55d6feb33d277a249de7e87a57ea1
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-05-17 18:38:06 +1000

    fixed output

--- iengine.py
+++ iengine.py
@@ -1,5 +1,5 @@
 import sys
-from inference_algorithms import tt_entails, fc_entails, bc_entails, dpll_entails, pl_resolution_entails
+from inference_algorithms import tt_entails, fc_entails, bc_entails, dpll_entails
 from expr import kb2expr, kb2expr_list, expr
 from utils import file_parser
 
@@ -15,6 +15,9 @@ def main():
 
     result_lhs, result_rhs = None, None
 
+    if method_name not in ["tt", "fc", "bc", "dpll"]:
+        raise ValueError("Incorrect algorithm name. Should be one of [tt, fc, bc, dpll]")
+
     if method_name == "tt":
         result_lhs, result_rhs = tt_entails(kb2expr(kb), expr(query))
     elif method_name == "fc":
@@ -22,21 +25,18 @@ def main():
     elif method_name == "bc":
         result_lhs, result_rhs = bc_entails(kb2expr_list(kb), expr(query))
     elif method_name == "dpll":
-        result_lhs = dpll_entails(kb2expr(kb), expr(query))
-        result_rhs = ''
-    elif method_name == "pl":
-        result_lhs = pl_resolution_entails(kb2expr_list(kb), expr(query))
-        result_rhs = ''
-    else:
-        raise RuntimeError("Incorrect command line arguments")
+        result_lhs, result_rhs = dpll_entails(kb2expr(kb), expr(query))
 
-
-    if result_lhs == True and result_rhs:
-        print("Yes: " + (str(result_rhs) if not isinstance(result_rhs, list) else ", ".join([x.op for x in result_rhs])))
-    elif result_lhs == True:
-        print("Yes")
-    else:
-        print('No')
+    if method_name in ["tt", "fc", "bc"]:
+        if result_lhs == True:
+            print("Yes: " + (str(result_rhs) if not isinstance(result_rhs, list) else ", ".join([x.op for x in result_rhs])))
+        else:
+            print('No')
+    elif method_name == "dpll":
+        if result_lhs == True:
+            print("Yes")
+        else:
+            print(f"No: {result_rhs}")
 
 
 if __name__ == '__main__':
